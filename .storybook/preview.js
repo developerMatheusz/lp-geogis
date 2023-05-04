e4902a836607cfc53f0be2672480c8f33721122b
Matheus Zanela <matheusdeveloper001@gmail.com>
2023-05-04 16:19:14 -0400

    last commit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,9 @@
-import { StoryFn } from "@storybook/addons";
 import { ThemeProvider } from "styled-components";
 import React from "react";
 import GlobalStyles from "styles/global";
 import theme from "styles/theme";
 
-const withGlobalStyle = (storyFn: StoryFn) => (
+const withGlobalStyle = (storyFn) => (
   <ThemeProvider theme={theme}>
     <GlobalStyles removeBg />
     {storyFn()}
