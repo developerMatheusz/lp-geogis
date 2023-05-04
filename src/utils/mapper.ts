11b44da72b87464c82dfb757d377095e77dc5f6a
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-05-04 10:32:39 -0300

    fix build

--- src/utils/mapper.ts
+++ src/utils/mapper.ts
@@ -1,22 +1,21 @@
-export const bannerMapper = (banners) => {
-  return banners.map((banner) => ({
+export const bannerMapper = (banners: any) => {
+  return banners.map((banner: any) => ({
     img: `http://localhost:1337${banner.attributes.img.data.attributes.url}`,
     title: banner.attributes.title,
     subtitle: banner.attributes.subtitle
   }));
 };
 
-export const newsInstagramMapper = (newsInstagram) => {
-  return newsInstagram.map((instagram) => ({
+export const newsInstagramMapper = (newsInstagram: any) => {
+  return newsInstagram.map((instagram: any) => ({
     title: instagram.attributes.title,
     img: `http://localhost:1337${instagram.attributes.img.data.attributes.url}`,
     description: instagram.attributes.description,
     href: instagram.attributes.href
-  }));
-};
+  }));};
 
-export const newsGeogisMapper = (newsGeogis) => {
-  return newsGeogis.map((geogis) => ({
+export const newsGeogisMapper = (newsGeogis: any) => {
+  return newsGeogis.map((geogis: any) => ({
     title: geogis.attributes.title,
     img: `http://localhost:1337${geogis.attributes.img.data.attributes.url}`,
     description: geogis.attributes.description
